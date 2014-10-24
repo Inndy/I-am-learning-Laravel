7bda59c3954d82a30017bdcaaa1e70e187af3121
Inndy <Inndy.mail@gmail.com>
2014-10-24 10:36:59 +0800

    Indent: 2 spaces

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="zh_TW">
 <head>
-    <meta charset="UTF-8">
-    <title>Laravel Site!</title>
-    <link rel="shortcut icon" href="/lab.ico" type="image/x-icon" />
+  <meta charset="UTF-8">
+  <title>Laravel Site!</title>
+  <link rel="shortcut icon" href="/lab.ico" type="image/x-icon" />
 </head>
 <body>
-    @yield('content')
+  @yield('content')
 </body>
 </html>
