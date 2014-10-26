4d643e6289c08ebc1fe2419ced80e390ecf34297
Inndy <Inndy.mail@gmail.com>
2014-10-27 03:09:12 +0800

    Bootstrap!

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -5,6 +5,9 @@
   <title>Laravel Site!</title>
   <link rel="shortcut icon" href="/lab.ico" type="image/x-icon" />
   <link rel="stylesheet" type="text/css" href="{{ asset('style/base.css') }}" media="all" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('bootstrap/css/bootstrap.min.css') }}" media="all" />
+  <script type="text/javascript" src="{{ asset('script/jquery-2.1.1.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
 </head>
 <body>
   <div class="main">
